80c4327115f13e0278af3ffe4e500cf765d30a91
dshantsev <49152616+dshantsev@users.noreply.github.com>
2021-12-08 10:54:06 +0100

    allow csv output

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 # main.py for DREF_PARSETAG
 
 from fastapi import FastAPI, Query, HTTPException
+from fastapi.responses import StreamingResponse
 from importlib import resources
+import io
+from enum import Enum
 
 from dref_parsing.parser_utils import *
 from dref_tagging.prediction import predict_tags_any_length
 
-
 app = FastAPI()
 
+# This Enum class allows us to see a dropdown menu with possible choices
+class OuputFormat(str, Enum):
+    json = "json"
+    csv = "csv"
 
-@app.get("/parse_and_tag/")
-async def parse_and_tag(
+
+@app.get("/parse_and_tag/{output_format}")
+async def parse_and_tag(output_format: OuputFormat, 
     Appeal_code: str = Query(
         'MDRDO013',
         title="Appeal code",
@@ -22,8 +29,9 @@ async def parse_and_tag(
     App for Parsing PDFs of DREF Final Reports and Tagging Excerpts.   
     <b>Input</b>: Appeal code of the report, MDR*****  
     <b>Output</b>:  
-    &nbsp;&nbsp; a dictionary of excerpts extracted from the PDF with its features: 'Learning', 'DREF_Sector',  
-    &nbsp;&nbsp; and global features: 'Hazard', 'Country', 'Date', 'Region', 'Appeal code'
+    &nbsp;&nbsp; a list of excerpts extracted from the PDF with its features: 'Learning', 'DREF_Sector',  
+    &nbsp;&nbsp; and global features: 'Hazard', 'Country', 'Date', 'Region', 'Appeal code'.  
+    &nbsp;&nbsp; The output can be given as a dictionary in json format, or as a csv file for download
 
     The app uses IFRC GO API to determine the global features (call 'appeal')  
     and to get the URL of the PDF report (call 'appeal_document')
@@ -39,6 +47,8 @@ async def parse_and_tag(
     # Renaming: In the program we call it 'lead', while IFRC calls it 'Appeal_code'
     lead = Appeal_code 
     
+    # ---------------------------------------------------------
+    # Parsing PDF
     try:
         # excerpts (and other relevant columns)
         all_parsed = parse_PDF_combined(lead)
@@ -51,36 +61,56 @@ async def parse_and_tag(
     except:
         raise HTTPException(status_code=500, detail="PDF Parsing didn't work by some reason")
 
-    df2 = all_parsed[['Modified Excerpt', 'Learning', 'DREF_Sector', 'lead', 'Hazard', 'Country', 'Date', 'Region']].copy() #,'position', 'DREF_Sector_id']]
+    df = all_parsed[['Modified Excerpt', 'Learning', 'DREF_Sector', 'lead', 'Hazard', 'Country', 'Date', 'Region']].copy() #,'position', 'DREF_Sector_id']]
 
-    # Do tagging
-    df2.loc[:,'Subdimension'] = df2['Modified Excerpt'].apply(lambda x: predict_tags_any_length(x)[0])
+    # -----------------------------------------------------------
+    # Tagging excerpts and cleaning/renaming
+
+    df.loc[:,'Subdimension'] = df['Modified Excerpt'].apply(lambda x: predict_tags_any_length(x)[0])
     # Split to "row per tag"
-    df2 = df2.explode('Subdimension')
+    df = df.explode('Subdimension')
 
     # Define Dimensions from Subdimensions using a dics from csv file
     with resources.path("dref_tagging.config", "DREF_spec.csv") as DREF_spec_file:
         spec = pd.read_csv(DREF_spec_file).set_index('Subdimension')    
     
-    df2['Dimension'] = df2['Subdimension'].apply(lambda x: spec.loc[x,'Dimension'] if x==x else np.nan)     
-    df2 = df2.fillna('Unknown')   
+    df['Dimension'] = df['Subdimension'].apply(lambda x: spec.loc[x,'Dimension'] if x==x else np.nan)     
+    df = df.fillna('Unknown')   
 
-    df2 = df2.rename(columns={'lead':'Appeal code','Modified Excerpt':'Excerpt'})
+    df = df.rename(columns={'lead':'Appeal code','Modified Excerpt':'Excerpt'})
 
     # reorder columns
     cols_order = ['Excerpt', 'Learning', 'DREF_Sector', 'Appeal code', 'Hazard', 'Country', 'Date', 'Region', 'Dimension' ,'Subdimension']
-    df2 = df2[cols_order]
+    df = df[cols_order]
 
-    return df2.to_dict()
+    # -----------------------------------
+    # Return DataFrame as Json or Csv:
 
-    # For CSV output a different conversion is needed 
-    return df2.to_csv(sep='|').split('\n')
+    if output_format == 'json':
+        return df.to_dict()
+
+    else: 
+        # prepare csv output
+        stream = io.StringIO()
+        # NB: comma as a separator works OK even if there exist commas in some excerpts 
+        # since pandas is smart to insert quotes where needed
+        df.to_csv(stream, index = False, sep=',')
+
+        response = StreamingResponse(iter([stream.getvalue()]), media_type="text/csv")
+        response.headers["Content-Disposition"] = "attachment; filename=export.csv"
+        return response    
 
 
-
+# *********************************************************************
 
 @app.get("/refresh/")
 async def reload_GO_API_data():
+    """
+    Reload data from GO database.  
+    This may be needed since the Parse-and-Tag app downloads data from GO
+    the first time it runs and never checks for updates.  
+    To refresh data from GO, run this app.
+    """    
     try:
         initialize_apdo(refresh=True)
         initialize_aadf(refresh=True)
