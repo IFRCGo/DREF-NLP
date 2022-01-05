c48a0d59a09cb5a4ea860c00f70e1e3dd5328a42
dshantsev <49152616+dshantsev@users.noreply.github.com>
2022-01-05 18:17:20 +0100

    allow for tiny test datasets

--- training_model/DocBERT/create_dref_data.py
+++ training_model/DocBERT/create_dref_data.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from pathlib import Path
+import sys
 
 
 def main():
+
     bert_dir = Path().parent.resolve()
     write_dir = bert_dir / "hedwig-data" / "datasets" / "DREF"
     write_dir.mkdir(parents=True, exist_ok=True)
@@ -14,6 +16,15 @@ def main():
     training = data.loc[data["Set"] == "training",cols]   
     development = data.loc[data["Set"] == "validation",cols]   
     test = data.loc[data["Set"] == "test",cols]
+
+    if len(sys.argv)>1:
+        # An additional parameter is used to limit the size of all datasets
+        # In order to run quick tests on small number of examples
+
+        n = int(sys.argv[1])
+        training = training[:n]
+        development = development[:n]
+        test = test[:n]
 
     training.to_csv(write_dir / "train.tsv", sep="\t", header=False, index=False)
     development.to_csv(write_dir / "dev.tsv", sep="\t", header=False, index=False)
