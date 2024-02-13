d255217e6edcb66ce3e917c4372873e194f34597
Alex Howes <howesalexandra@gmail.com>
2024-02-13 13:46:38 +0100

    Change img to contained instead of overlap

--- utils.py
+++ utils.py
@@ -11,10 +11,12 @@ def extract_text_and_fontsizes(document_path):
     total_y = 0
 
     # Loop through pages and paragraphs
-    for page_number, page_layout in enumerate(fitz.open(document_path)):
+    doc = fitz.open(document_path)
+    for page_number, page_layout in enumerate(doc):
 
         # Get drawings to get text highlights
         coloured_drawings = [drawing for drawing in page_layout.get_drawings() if (drawing['fill'] != (0.0, 0.0, 0.0))]
+        page_images = page_layout.get_image_info()
 
         # Loop through blocks
         blocks = page_layout.get_text("dict", flags=11)["blocks"]
@@ -37,9 +39,17 @@ def extract_text_and_fontsizes(document_path):
                         highlight_color = largest_highlight['fill']
                         if highlight_color:
                             highlight_color_hex = '#%02x%02x%02x' % (int(255*highlight_color[0]), int(255*highlight_color[1]), int(255*highlight_color[2]))
+
+                    # Check if the span overlaps with an image
+                    max_overlap = None
+                    overlapping_images = [get_overlap(span['bbox'], img['bbox'])/get_area(span['bbox']) for img in page_images if get_overlap(span['bbox'], img['bbox'])]
+                    if overlapping_images:
+                        max_overlap = max(overlapping_images)
+
+                    contains_images = [img for img in page_images if contains(img['bbox'], span['bbox'])]
                     
                     # Append results
-                    span['text'] = span['text'].strip().replace('\r', '\n')
+                    span['text'] = span['text'].replace('\r', '\n')
                     span['bold'] = is_bold(span["font"])
                     span['highlight_color'] = highlight_color_hex
                     span['page_number'] = page_number
@@ -49,6 +59,7 @@ def extract_text_and_fontsizes(document_path):
                     span['origin_x'] = span['origin'][0]
                     span['origin_y'] = span['origin'][1]
                     span['total_y'] = span['origin'][1]+total_y
+                    span['img'] = bool(contains_images)
                     data.append(span)
 
         total_y += page_layout.rect.height
@@ -77,12 +88,28 @@ def is_bold(text):
     return False
 
 
+def contains(bbox1, bbox2):
+    # Check if bbox1 contains bbox2
+    if (
+        (bbox1[0] < bbox2[0]) and 
+        (bbox1[1] < bbox2[1]) and 
+        (bbox1[2] > bbox2[2]) and 
+        (bbox1[3] > bbox2[3])
+    ):
+        return True
+    return False
+
+
 def get_overlap(bbox1, bbox2):
     # Get overlap area between boxes
     dx = min(bbox1[2], bbox2[2]) - max(bbox1[0], bbox2[0])
     dy = min(bbox1[3], bbox2[3]) - max(bbox1[1], bbox2[1])
     if (dx >= 0) and (dy >= 0):
         return dx*dy
+
+
+def get_area(bbox):
+    return abs((bbox[2]-bbox[0])*(bbox[1]-bbox[3]))
 
 
 def is_text_title(text):
