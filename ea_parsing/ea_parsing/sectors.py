266e33181f3d2a30071b0e660065f5ec047b460a
Alex Howes <howesalexandra@gmail.com>
2024-02-27 21:03:02 +0100

    Remove keywords from sector similarity analysis as not really used

--- ea_parsing/ea_parsing/sectors.py
+++ ea_parsing/ea_parsing/sectors.py
@@ -26,39 +26,22 @@ class Sectors:
     def _strip_characters(self, sectors):
         # Process - add sector name, and swap common words
         sectors_processed = {}
-        for sector_name, details in sectors.items():
-            sectors_processed[sector_name] = {}
+        for sector_name, titles in sectors.items():
 
             # Add the sector name to the titles
             sector_name_base = strip_non_alpha(sector_name).lower()
-            if details is None:
-                sectors_processed[sector_name] = {
-                    'titles': [sector_name_base],
-                    'keywords': []
-                }
+            if titles is None:
+                sectors_processed[sector_name] = [sector_name_base]
             else:
-                if 'titles' in details:
 
-                    # Loop through titles and add base text
-                    sectors_processed[sector_name]['titles'] = [
-                        strip_non_alpha(title).lower() 
-                        for title in details['titles']
-                    ]
-                    # Add sector title
-                    if sector_name_base not in details['titles']:
-                        sectors_processed[sector_name]['titles'].append(sector_name_base)
-
-                else:
-                    sectors_processed[sector_name]['titles'] = [sector_name_base]
-
-                # Loop through keywords and add base text
-                if 'keywords' in details:
-                    sectors_processed[sector_name]['keywords'] = [
-                        strip_non_alpha(keyword).lower() 
-                        for keyword in details['keywords']
-                    ]
-                else:
-                    sectors_processed[sector_name]['keywords'] = []
+                # Loop through titles and add base text
+                sectors_processed[sector_name] = [
+                    strip_non_alpha(title).lower() 
+                    for title in titles
+                ]
+                # Add sector title
+                if sector_name_base not in titles:
+                    sectors_processed[sector_name].append(sector_name_base)
 
         return sectors_processed
 
@@ -67,17 +50,14 @@ class Sectors:
         """
         """
         sectors_with_abbs = {}
-        for sector_name, details in sectors.items():
-            sectors_with_abbs[sector_name] = {
-                'titles': [], 
-                'keywords': details['keywords']
-            }
-            for title in details['titles']:
+        for sector_name, titles in sectors.items():
+            sectors_with_abbs[sector_name] = []
+            for title in titles:
                 sector_options = generate_sentence_variations(
                     sentence=title, 
                     abbreviations=abbreviations
                 )
-                sectors_with_abbs[sector_name]['titles'] += sector_options
+                sectors_with_abbs[sector_name] += sector_options
 
         return sectors_with_abbs
 
@@ -97,46 +77,15 @@ class Sectors:
                     text_base = text_base[len(prefix):].strip()
 
         # First, check if there is an exact match with the titles
-        for sector_name, details in self.sectors.items():
-            for title in details['titles']:
+        for sector_name, titles in self.sectors.items():
+            for title in titles:
                 if text_base == title:
                     return sector_name, 1
 
         # Next, check if the title is any title plus filler words 
-        for sector_name, details in self.sectors.items():
-            for title in details['titles']:
+        for sector_name, titles in self.sectors.items():
+            for title in titles:
                 if strip_filler_words(text_base) == strip_filler_words(title):
                     return sector_name, 1
-
-        # Next, check if there is an exact match with any keywords
-        for sector_name, details in self.sectors.items():
-            for keyword in details['keywords']:
-                if text_base == keyword:
-                    return sector_name, 0.9
-
-        # Next, check how many words in the text are covered by each sector and sector keywords
-        proportion_text_covered_by_sector = {}
-        for sector_name, details in self.sectors.items():
-            keywords = details['keywords']
-            
-            # Extract filler words and keywords
-            text_without_fillers = strip_filler_words(text_base)
-            text_without_keywords = replace_phrases_in_sentence(keywords, '', text_without_fillers).strip()
-
-            # Get the proportion of words covered
-            if text_without_fillers == text_without_keywords:
-                proportion_text_covered_by_sector[sector_name] = 0
-            elif not text_without_keywords:
-                proportion_text_covered_by_sector[sector_name] = 1
-            else:
-                number_words_covered = len(text_without_fillers.split(' ')) - len(text_without_keywords.split(' '))
-                proportion_text_covered_by_sector[sector_name] = number_words_covered/len(text_without_fillers.split(' '))
-
-        # Return the best matching sector
-        max_sector = max(proportion_text_covered_by_sector, key=lambda x: 0 if proportion_text_covered_by_sector[x]!=proportion_text_covered_by_sector[x] else proportion_text_covered_by_sector[x])
-        max_proportion = proportion_text_covered_by_sector[max_sector]
-
-        if max_proportion > 0:
-            return max_sector, max_proportion
 
         return None, 0
