ab576cfa5e678f131be6d0beee4d4d7435dea92e
Alex Howes <howesalexandra@gmail.com>
2024-03-08 18:32:22 +0100

    Add challenges results. Don't get results if already saved, if no overwrite.

--- ea_parsing/tests/extract_lines.py
+++ ea_parsing/tests/extract_lines.py
@@ -8,10 +8,19 @@ parser.add_argument('-o', "--overwrite", action='store_true')
 parser.add_argument('-no', "--no_overwrite", action='store_true')
 args = parser.parse_args()
 
-# Read in the lessons learned results from yaml files
+# Get the paths to results files
 lessons_learned_results = {}
 TESTS_DIR = os.path.dirname(os.path.realpath(__file__))
-files = os.listdir(os.path.join(TESTS_DIR, 'results'))
+challenge_results_files = os.listdir(os.path.join(TESTS_DIR, 'challenges_results'))
+lessons_learned_results_files = os.listdir(os.path.join(TESTS_DIR, 'lessons_learned_results'))
+files = list(set(challenge_results_files + lessons_learned_results_files))
+
+# If no overwrite, don't bother getting files which are already saved
+if args.no_overwrite:
+    raw_lines_saved = os.listdir(os.path.join(TESTS_DIR, 'raw_lines'))
+    files = [file for file in files if file not in raw_lines_saved]
+
+# Loop through files and get final report lines
 for file in files:
     mdr_code = os.path.splitext(file)[0]
 
@@ -31,6 +40,6 @@ for file in files:
                 if save_results in ['y', 'n']:
                     break
 
-    # Save the results
+    # Save the final report lines
     if save_results=='y' or args.overwrite or not os.path.isfile(document_lines_path):
         final_report.raw_lines.to_csv(document_lines_path, index=True)
