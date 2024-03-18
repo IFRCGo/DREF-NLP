a4860bf144ffeb1364d0951376e5fdad24c0da87
Alex Howes <howesalexandra@gmail.com>
2024-03-18 23:28:37 +0100

    Add diamonds as bullet points

--- ea_parsing/ea_parsing/definitions.py
+++ ea_parsing/ea_parsing/definitions.py
@@ -10,8 +10,9 @@ ABBREVIATIONS = yaml.safe_load(open(os.path.join(DEFINITIONS_DIR, 'abbreviations
 
 BULLETS = [
     '•', '●', '▪', '-', 'o',
+    '❖', '◆', '♢', '◇', '⬖',
     u'\uf0b7', u'\u2023', u'\u2043', u'\u204C', u'\u204D', u'\u2219',
     u'\u25CB', u'\u25CF', u'\u25D8', u'\u25E6', u'\u2619', u'\u2765',
-    u'\2767', u'\u29BE', u'\u29BF', u'\u25C9', u'\uf0a7', u'\uf0d8', 
+    u'\2767', u'\u29BE', u'\u29BF', u'\u25C9', u'\uf0a7', u'\uf0d8',
     u'\uf076'
 ]
