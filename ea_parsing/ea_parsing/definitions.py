3552f34b0df82ecad457908d22721c3226957267
Alex Howes <howesalexandra@gmail.com>
2024-03-18 17:13:21 +0100

    Move bullet list to definitions

--- ea_parsing/ea_parsing/definitions.py
+++ ea_parsing/ea_parsing/definitions.py
@@ -7,3 +7,10 @@ DEFINITIONS_DIR = os.path.join(ROOT_DIR, 'ea_parsing', 'definitions')
 LESSONS_LEARNED_TITLES = yaml.safe_load(open(os.path.join(DEFINITIONS_DIR, 'lessons_learned_titles.yml')))
 SECTORS = yaml.safe_load(open(os.path.join(DEFINITIONS_DIR, 'sectors.yml')))
 ABBREVIATIONS = yaml.safe_load(open(os.path.join(DEFINITIONS_DIR, 'abbreviations.yml')))
+
+BULLETS = [
+    '•', '●', '▪', '-', 'o',
+    u'\uf0b7', u'\u2023', u'\u2043', u'\u204C', u'\u204D', u'\u2219',
+    u'\u25CB', u'\u25CF', u'\u25D8', u'\u25E6', u'\u2619', u'\u2765',
+    u'\2767', u'\u29BE', u'\u29BF', u'\u25C9'
+]
--- ea_parsing/ea_parsing/lines.py
+++ ea_parsing/ea_parsing/lines.py
@@ -4,6 +4,7 @@ from ast import literal_eval
 import re
 from functools import cached_property
 import pandas as pd
+import ea_parsing.definitions
 from ea_parsing.utils import colour_diff, is_text_title
 
 
@@ -122,15 +123,6 @@ class Lines(pd.DataFrame):
                             self['color'].astype(str)+', ' +\
                             self['highlight_color'].astype(str)
 
-    @cached_property
-    def bullet_chars(self):
-        return [
-            '•', '●', '▪', '-',
-            u'\uf0b7', u'\u2023', u'\u2043', u'\u204C', u'\u204D', u'\u2219',
-            u'\u25CB', u'\u25CF', u'\u25D8', u'\u25E6', u'\u2619', u'\u2765',
-            u'\2767', u'\u29BE', u'\u29BF', u'\u25C9'
-        ]
-
     @property
     def _constructor(self):
         return Lines
@@ -171,7 +163,7 @@ class Lines(pd.DataFrame):
 
         # Get bullets: span zero, bullet character
         bullets = lines.loc[
-            (lines['text'].str.strip().isin(self.bullet_chars)) &
+            (lines['text'].str.strip().isin(ea_parsing.definitions.BULLETS)) &
             (lines['span_number'] == 0)
         ]
 
@@ -332,7 +324,7 @@ class Lines(pd.DataFrame):
         # Get which lines start with a bullet
         lines['bullet'] = False
         lines.loc[
-            (lines['text'].str.strip().isin(self.bullet_chars)) &
+            (lines['text'].str.strip().isin(ea_parsing.definitions.BULLETS)) &
             (lines['span_number'] == 0),
             'bullet'
         ] = True
