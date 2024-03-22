56325d8e8b7e1e1e613ece7227791c2471142b79
Alex Howes <howesalexandra@gmail.com>
2024-03-22 12:17:12 +0100

    Change regex pattern as sometimes mdr codes can be long

--- ea_parsing/ea_parsing/main.py
+++ ea_parsing/ea_parsing/main.py
@@ -12,7 +12,7 @@ async def run_parsing(
         description="Starts with 'MDR' followed by 5 symbols. <br> Some available codes: DO013, BO014, CL014, AR017, VU008, TJ029, SO009, PH040, RS014, FJ004, CD031, MY005, LA007, CU006, AM006",
         min_length=8,
         max_length=8,
-        regex="^MDR[A-Z0-9]{5}$"
+        regex="^MDR[A-Z0-9]{5,7}$"
         )
 ):
     """
